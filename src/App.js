9a08940c94fd22e3ed1171413b654d250e71d89d
hossainmdrasel999@gmail.com <Rasel-Hossain999>
2022-10-03 08:36:23 -0700

    Product Screen Finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { Container } from 'react-bootstrap';
 import ProductScreen from './screens/ProductScreen';
 
@@ -18,6 +19,7 @@ const App = () => {
           </Routes>        
           </Container>
         </main>
+      <Footer/>
     </BrowserRouter>
   )
 }
