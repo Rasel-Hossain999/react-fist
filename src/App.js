fa368329f61d43eb21c8dda6f96dd4202882d4f9
hossainmdrasel999@gmail.com <Rasel-Hossain999>
2022-10-03 05:27:51 -0700

    product screen error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header'
 import { Container } from 'react-bootstrap';
+import ProductScreen from './screens/ProductScreen';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <Container>
           <Routes>
               <Route path='/' element={<HomeScreen/>}></Route>
+              <Route path='/product/:id' element={<ProductScreen/>}></Route>
           </Routes>        
           </Container>
         </main>
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import products from '../products'
+
+const ProductScreen = ({ match }) => {
+    const product = products.find((p) => p._id === match.params.id)
+  return (
+      <div>{product.name}</div>
+  )
+  
+}
+
+export default ProductScreen
